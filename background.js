1bf4d9eabe9f5c86dd760a0847a97aa779495b5f
AlexMedoff <alexfmedoff@gmail.com>
2017-12-06 13:59:39 -0500

    f

---
+++ background.js
@@ -0,0 +1,4 @@
+//Gets all elemnts with style atribute
+var elms = document.querySelectorAll("*[style]");
+var lastMoved = document.getElementsByClassName('last-move');
+var whitePawns = document.getElementsByClassName('white pawn');

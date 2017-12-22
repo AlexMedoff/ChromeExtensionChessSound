203613a51e43c16207caaa3fbcb312252ab07ec7
AlexMedoff <alexfmedoff@gmail.com>
2017-12-22 18:47:40 -0500

    console logs moves

--- player.js
+++ player.js
@@ -1,9 +1,21 @@
 var from = null;
 var lastMove;
 
+//Mutation Observer Will analyze mutations and make sense of what move is being made
+
 var boardObserver = new MutationObserver(function(mutations) {
 	mutations.forEach(function(mutation) {
-		console.log("MUTATION OBSERVED");
+
+		var mutationTarget = mutation.target;		//Makes things simpler when I want to reference the target of the Node
+
+		if (mutationTarget.className == "moves") {		//Only care about mutations that are moves on the board
+			
+			//console.log(mutation);
+
+			var moveArray = mutationTarget.childNodes;
+
+			console.log(moveArray[moveArray.length-2].innerText);
+		}
 	});
 });
 

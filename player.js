1638c41aaaf6040dd989c6ec301e4cd85e93c115
AlexMedoff <alexfmedoff@gmail.com>
2017-12-29 20:18:57 -0500

    trouble

--- player.js
+++ player.js
@@ -2,24 +2,40 @@ var from = null;
 var lastMove;
 
 var take = false;
+var check = false;
 
 //Mutation Observer Will analyze mutations and make sense of what move is being made
 
 var boardObserver = new MutationObserver(function(mutations) {
+
+
+	/****** CHECKING FOR CHECKS OR TAKES *****/
+
+	console.log(mutations.length);
+	
+	if (mutations.length == 3) {
+		if (mutations[2].target.className == 'cg-board' && mutations[2].target.removedNodes.length > 0) {
+			take = true;
+		}
+		else if (mutations[2].target.className == 'cg-board' && mutations[2].target.removedNodes.length > 0) {
+			check = true;
+		}
+	}
+	else {
+		take = false;
+		check = false;
+	}
+
+	/****** IDENTIFYING THE MOVE *****/
+
 	mutations.forEach(function(mutation) {
 
 		var mutationTarget = mutation.target;		//Makes things simpler when I want to reference the target of the Node
 
-		//console.log(mutationTarget);
-
-		if (mutationTarget.className == 'cg-board' && mutation.removedNodes.length > 0) {
-			console.log(mutation);
-		}
-
-		if (mutationTarget.className == 'last-move' && mutationTarget.clientHeight == 64) {
-			//console.log(mutationTarget.attributes.style);
-			//console.log(mutation);
-			
+		if (mutationTarget.className == 'last-move' && mutationTarget.clientHeight == 64) {			//attributes (moving peices (comes in pairs of two))
+			console.log(mutationTarget.attributes.style);
+			console.log(take);
+			console.log(check);
 		}
 
 		// if (mutationTarget.className == "moves") {		//Only care about mutations that are moves on the board

2ade184f44d92990e3c97aebd5819f2a093f67d1
AlexMedoff <alexfmedoff@gmail.com>
2018-08-27 17:52:30 -0400

    Added code that tracks the difference variable

--- player.js
+++ player.js
@@ -1,11 +1,12 @@
-var from = null;
-var lastMove;
+var lastSquare = -1;
+var nextSquare = -1;
+
+var difference = 0;
 
 var take = false;
 var check = false;
 
 //Mutation Observer Will analyze mutations and make sense of what move is being made
-
 var boardObserver = new MutationObserver(function(mutations) {
 
 	mutations.forEach(function(mutation) {
@@ -27,7 +28,19 @@ var boardObserver = new MutationObserver(function(mutations) {
 
 		/****** IDENTIFYING THE MOVE *****/
 		if (mutationTarget.className == 'last-move' && mutationTarget.clientHeight == 64) {			//attributes (moving peices (comes in pairs of two))
-			//console.log(mutationTarget.attributes.style);
+			var full_string_translate = mutationTarget.outerHTML;	//Getting a string of the whole translate html
+			var index1 = (full_string_translate.indexOf(",") + 2);	//Findinx index of beggining of Y translation
+			var index2 = (full_string_translate.indexOf(")") - 2);		//Finding index of end of Y translation
+			var YPos = full_string_translate.slice(index1, index2);		//Slicing the full string so that now I just have the Ytranslation
+			console.log(YPos);
+			if (lastSquare < 0) {
+				lastSquare = YPos;
+			}
+			else {
+				nextSquare = YPos;
+				difference = (nextSquare - lastSquare);
+				console.log("The difference is " + difference);
+			}
 			console.log(mutation);
 		}
 
@@ -42,6 +55,7 @@ var boardObserver = new MutationObserver(function(mutations) {
 	if (take) { console.log("TAKE!"); }
 });
 
+//Board Observer observing the page
 boardObserver.observe(document.documentElement, {
 	childList: true,
 	subtree: true,
@@ -49,4 +63,11 @@ boardObserver.observe(document.documentElement, {
     attributeFilter: ["style"]
 });
 
-console.log("running");
+//Move Class
+function Move(i_take, i_check, i_checkmate, i_piece, i_difference) {
+	this.takeM = i_take;
+	this.checKM = i_check;
+	this.checkMate = i_checkmate;
+	this.pieceM = i_piece;
+	this.differenceM = i_difference; 
+}

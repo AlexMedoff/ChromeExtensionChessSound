77ea9fd3d77c5ad2f9d04359305eaed4c49ae7af
AlexMedoff <alexfmedoff@gmail.com>
2018-08-31 21:15:30 -0400

    redid everything

--- player.js
+++ player.js
@@ -6,6 +6,9 @@ var difference = 0;
 var take = false;
 var check = false;
 
+var pieceType;
+var color;
+
 //Mutation Observer Will analyze mutations and make sense of what move is being made
 var boardObserver = new MutationObserver(function(mutations) {
 
@@ -13,47 +16,27 @@ var boardObserver = new MutationObserver(function(mutations) {
 
 		var mutationTarget = mutation.target;		//Makes things simpler when I want to reference the target of the Node
 
-		/********* CHECKS AND TAKES ********/
-		if (mutationTarget.className == 'cg-board' && mutation.removedNodes.length > 0) {
-			take = true;
-		}
-		else { take = false; }
-
-		if (mutationTarget.className == 'cg-board' && mutation.addedNodes.length > 0) {
-			check = true;
-			console.log(mutation);
-		}
-		else { check = false; }
-
-
-		/****** IDENTIFYING THE MOVE *****/
-		if (mutationTarget.className == 'last-move' && mutationTarget.clientHeight == 64) {			//attributes (moving peices (comes in pairs of two))
-			var full_string_translate = mutationTarget.outerHTML;	//Getting a string of the whole translate html
-			var index1 = (full_string_translate.indexOf(",") + 2);	//Findinx index of beggining of Y translation
-			var index2 = (full_string_translate.indexOf(")") - 2);		//Finding index of end of Y translation
-			var YPos = full_string_translate.slice(index1, index2);		//Slicing the full string so that now I just have the Ytranslation
-			console.log(YPos);
-			if (lastSquare < 0) {
-				lastSquare = YPos;
+			/******* CHECKS AND TAKES *****/
+			if (mutationTarget.className == 'cg-board' && mutation.removedNodes.length > 0 && mutation.removedNodes[0].nodeName == "PIECE") {
+				take = true;
 			}
-			else {
-				nextSquare = YPos;
-				difference = (nextSquare - lastSquare);
-				console.log("The difference is " + difference);
+			if (mutationTarget.className == 'cg-board' && mutation.addedNodes.length > 0 && mutation.addedNodes[0].className == "check") {
+				check = true;
 			}
-			console.log(mutation);
-		}
+			if (mutationTarget.nodeName == 'PIECE') {
+				if (!mutationTarget.className.includes("dragging") && !mutationTarget.className.includes("ghost") && !mutationTarget.className.includes("anim")) {
+					console.log(mutationTarget.className);
+					var stringArr = mutationTarget.className.split(" ");
+					color = stringArr[0];
+					pieceType = stringArr[1];
+					console.log("The color is " + color);
+					console.log("The piece is " + pieceType);
+				}
+			}
 
-		// if (mutationTarget.className == "moves") {		//Only care about mutations that are moves on the board
-			
-		// 	//console.log(mutation);
-		// 	var moveArray = mutationTarget.childNodes;
-		// 	console.log(moveArray[moveArray.length-2].innerText);
-		// }
 	});
-	if (check) { console.log("CHECK!"); }
-	if (take) { console.log("TAKE!"); }
 });
+
 
 //Board Observer observing the page
 boardObserver.observe(document.documentElement, {
